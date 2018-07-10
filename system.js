8661f9ec3cb1708375ce29e90a327f2cac08346f
Rachmizard <rachmizard11072000@gmail.com>
2018-07-10 22:55:37 +0700

    Fix tinggal ngasih animasi dan ngatur ngatur

--- system.js
+++ system.js
@@ -47,147 +47,168 @@
 
 // // });
 
+$('#modalInputan').modal();
+$('#modalInputan').modal('open');
+$('#modalInputan').addClass('animated tada');
 
+function cobaInputan(){
+	$('#modalInputan').modal();
+	$('#modalInputan').modal('close');
 
-// retrieving guest list
-var db = firebase.database();
+	Materialize.toast('Memuat halaman...', 1200, 'rounded');
+	// checking data if exist or not
+	var ref = firebase.database().ref('/'+ document.getElementById('kode').value);
+	ref.once("value")
+	  .then(function(snapshot) {
+	    if (!snapshot.exists()) {
+	    	if (confirm('Data guest kosong pada kode ini, lanjut?')) {
+	    		alert('Masukan kode '+ document.getElementById('kode').value +' di aplikasi guest list Android mu!');
+	    	}else{
+	    		alert('Kode di batalkan, silahkan masukan kode yang sudah tersedia.');
+	    		location.reload();
+	    	}
+	    }
+	  });
+	// retrieving guest list
+	var db = firebase.database();
+	var retrieve = document.getElementById('retrieve');
+	var retrieveRef = db.ref('/'+ document.getElementById('kode').value);
+		retrieveRef.on('child_added', (data) => {
+		//counting data
+		firebase.database().ref('/'+ document.getElementById('kode').value).on("value", function(snapshot) {
+		  console.log("There are "+snapshot.numChildren()+" guest");
+		});
+		  var tr = document.createElement('div');
+		  tr.className = "background-primary";
+		  tr.id = data.key;
+		  tr.innerHTML = reviewTemplateProdi(data.val());
+		  retrieve.append(tr);
+		  setTimeout(function(){
+       		window.scrollTo(0,document.body.scrollHeight);
+		  	$(tr).addClass('animated fadeIn');
+		  }, 1500)
+		});
 
-var retrieve = document.getElementById('retrieve');
-var retrieveRef = db.ref('/guest');
+		retrieveRef.on('child_changed', (data) => {
+		  var reviewNode = document.getElementById(data.key);
+		  reviewNode.innerHTML = reviewTemplateProdi(data.val());
+		});
 
-retrieveRef.on('child_added', (data) => {
-//counting data
-firebase.database().ref("guest/").on("value", function(snapshot) {
-  console.log("There are "+snapshot.numChildren()+" guest");
-});
-  var tr = document.createElement('div');
-  tr.className = "background-primary";
-  tr.id = data.key;
-  tr.innerHTML = reviewTemplateProdi(data.val());
-  retrieve.append(tr);
-  window.scroll({top: 900000000000000000000000000000, behavior: 'smooth'});
-});
+		retrieveRef.on('child_removed', (data) => {
+		  var reviewNode = document.getElementById(data.key);
+		  reviewNode.parentNode.removeChild(reviewNode);
+		});
 
-retrieveRef.on('child_changed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.innerHTML = reviewTemplateProdi(data.val());
-});
-
-retrieveRef.on('child_removed', (data) => {
-  var reviewNode = document.getElementById(data.key);
-  reviewNode.parentNode.removeChild(reviewNode);
-});
-
-function reviewTemplateProdi({foto, nama, foto1, nama1, status}) {
-  		if (status == 'Single') {
-  			return `
-				<header>
-				    <div class="head-bg center">
-				        <img src='asset/img/brush_screen.png' alt="">
-				        <div class="head-text">
-				            <p awal>Thank you for coming</p>
-				            <p akhir>to our party</p>
-				        </div><!--/head text-->
-				    </div><!--/.head bg-->
-				</header>
-				<main>
-				    <div class="container-material">
-				    <div class="row">
-				        <div class="col-sm-12 col-xs-12 center">
-				            <div class="con-user solo">
-				                <div class="animationmuter"></div><!--/.animation muter-->
-				                <div class="con-img">
-				                    <img src="${foto}" alt="Augusta Albertus" class="img-con">
-				                    <span class="name">${nama}</span><!--/.name-->
-				                </div><!--/.conimg-->
-				            </div><!--/.conuser-->
-				        </div><!--col-->
-				    </div><!--/.row-->
-				    </div><!--/contanier-->
-				</main>
-				<footer>
-				    <div class="container-material">
-				    <div class="row">
-				        <div class="col-xs-12 center">
-				            <p x-mas>
-				                <span class="niik">Jimmy</span>
-				                <img src="asset/img/wedding.png" alt="">
-				                <span class="niik">Wulan</span>
-				            </p>
-				        </div><!--/.col-->
-				        <div class="col-xs-12 center">
-				            <p>
-				                <span class="copyright1">Original Concept by : </span>
-				                <span class="copyright2">Jimmy Yogaswara | Birutekno inc.</span>
-				            </p>
-				        </div><!--/.col-->
-				    </div><!--/.row-->
-				    </div><!--/.container-->
-				</footer>
-				<!--/STOP REPEAT-->
-	  		`
-  		}else
-  		{
-  			return `
-  				<header>
-				    <div class="head-bg center">
-				        <img src='asset/img/brush_screen.png' alt="">
-				        <div class="head-text">
-				            <p awal>Thank you for coming</p>
-				            <p akhir>to our party</p>
-				        </div><!--/head text-->
-				    </div><!--/.head bg-->
-				</header>
-				<main>
-				    <div class="container-material">
+	function reviewTemplateProdi({foto, nama, foto1, nama1, status}) {
+	  		if (status == 'Single') {
+	  			return `
+					<header>
+					    <div class="head-bg center">
+					        <img src='asset/img/brush_screen.png' alt="">
+					        <div class="head-text white-text">
+					            <p awal>Thank you for coming</p>
+					            <p akhir>to our party</p>
+					        </div><!--/head text-->
+					    </div><!--/.head bg-->
+					</header>
+					<main>
+					    <div class="container-material">
 					    <div class="row">
-					        <div class="col-sm-4 col-xs-4 center">
-					            <div class="con-user">
+					        <div class="col-sm-12 col-xs-12 center">
+					            <div class="con-user solo">
 					                <div class="animationmuter"></div><!--/.animation muter-->
 					                <div class="con-img">
-					                    <img src="${foto}" alt="${nama}" class="img-con">
-					                    <span class="name">Mr. ${nama}</span><!--/.name-->
+					                    <img src="${foto}" alt="Augusta Albertus" class="img-con">
+					                    <span class="name">${nama}</span><!--/.name-->
 					                </div><!--/.conimg-->
-					            </div><!--/.conuser-->
-					        </div><!--col-->
-					        <div class="col-sm-4 col-xs-4 center">
-					            <h1 codename>&amp;</h1>
-					        </div><!--col-->
-					        <div class="col-sm-4 col-xs-4 center">
-					            <div class="con-user">
-					                <div class="animationmuter"></div><!--/.animation muter-->
-					                <div class="con-img">
-					                    <img src="${foto1}" alt="${nama1}" class="img-con">
-					                     <span class="name">Mrs. ${nama1}</span><!--/.name-->
-					                </div><!--/.conimg-->
-					               
 					            </div><!--/.conuser-->
 					        </div><!--col-->
 					    </div><!--/.row-->
-				    </div><!--/.container-->
-				
-				<footer>
-				    <div class="container-material">
-				    <div class="row">
-				        <div class="col-xs-12 center">
-				            <p x-mas>
-				                <span class="niik">Jimmy</span>
-				                <img src="asset/img/wedding.png" alt="">
-				                <span class="niik">Wulan</span>
-				            </p>
-				        </div><!--/.col-->
-				        <div class="col-xs-12 center">
-				            <p>
-				                <span class="copyright1">Original Concept by : </span>
-				                <span class="copyright2">Jimmy Yogaswara | Birutekno inc.</span>
-				            </p>
-				        </div><!--/.col-->
-				    </div><!--/.row-->
-				    </div><!--/.container-->
-				</footer>
-				<!--/STOP REPEAT-->
-				<!--/STOP REPEAT-->
-  			`
-  		}
-};
-
+					    </div><!--/contanier-->
+					</main>
+					<footer>
+					    <div class="container-material">
+					    <div class="row">
+					        <div class="col-xs-12 center">
+					            <p x-mas>
+					                <span class="niik">Jimmy</span>
+					                <img src="asset/img/wedding.png" alt="">
+					                <span class="niik">Wulan</span>
+					            </p>
+					        </div><!--/.col-->
+					        <div class="col-xs-12 center">
+					            <p>
+					                <span class="copyright1">Original Concept by : </span>
+					                <span class="copyright2">Jimmy Yogaswara | Birutekno inc.</span>
+					            </p>
+					        </div><!--/.col-->
+					    </div><!--/.row-->
+					    </div><!--/.container-->
+					</footer>
+					<!--/STOP REPEAT-->
+		  		`
+	  		}else if(status = 'Couple')
+	  		{
+	  			return `
+	  				<header>
+					    <div class="head-bg center">
+					        <img src='asset/img/brush_screen.png' alt="">
+					        <div class="head-text">
+					            <p awal>Thank you for coming</p>
+					            <p akhir>to our party</p>
+					        </div><!--/head text-->
+					    </div><!--/.head bg-->
+					</header>
+					<main>
+					    <div class="container-material">
+						    <div class="row">
+						        <div class="col-sm-4 col-xs-4 center">
+						            <div class="con-user">
+						                <div class="animationmuter"></div><!--/.animation muter-->
+						                <div class="con-img">
+						                    <img src="${foto}" alt="${nama}" class="img-con">
+						                    <span class="name">Mr. ${nama}</span><!--/.name-->
+						                </div><!--/.conimg-->
+						            </div><!--/.conuser-->
+						        </div><!--col-->
+						        <div class="col-sm-4 col-xs-4 center">
+						            <h1 codename>&amp;</h1>
+						        </div><!--col-->
+						        <div class="col-sm-4 col-xs-4 center">
+						            <div class="con-user">
+						                <div class="animationmuter"></div><!--/.animation muter-->
+						                <div class="con-img">
+						                    <img src="${foto1}" alt="${nama1}" class="img-con">
+						                     <span class="name">Mrs. ${nama1}</span><!--/.name-->
+						                </div><!--/.conimg-->
+						               
+						            </div><!--/.conuser-->
+						        </div><!--col-->
+						    </div><!--/.row-->
+					    </div><!--/.container-->
+					
+					<footer>
+					    <div class="container-material">
+					    <div class="row">
+					        <div class="col-xs-12 center">
+					            <p x-mas>
+					                <span class="niik">Jimmy</span>
+					                <img src="asset/img/wedding.png" alt="">
+					                <span class="niik">Wulan</span>
+					            </p>
+					        </div><!--/.col-->
+					        <div class="col-xs-12 center">
+					            <p>
+					                <span class="copyright1">Original Concept by : </span>
+					                <span class="copyright2">Jimmy Yogaswara | Birutekno inc.</span>
+					            </p>
+					        </div><!--/.col-->
+					    </div><!--/.row-->
+					    </div><!--/.container-->
+					</footer>
+					<!--/STOP REPEAT-->
+					<!--/STOP REPEAT-->
+	  			`
+	  		}
+	};	
+}
